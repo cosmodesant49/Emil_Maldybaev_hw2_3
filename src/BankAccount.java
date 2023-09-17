1cedfd1a3d6500b87f5d0b88882d68b0839b5392
cosmodesant49 <emilmaldybaev49@gmail.com>
2023-09-17 23:07:26 +0600

    v1

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class BankAccount {
+public class BankAccount {
+    private double amount;
+
+    public BankAccount(double initialAmount) {
+        this.amount = initialAmount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+    public void deposit(double sum) {
+        if (sum > 0) {
+            amount += sum;
+            System.out.println("Баланс пополнен на " + sum + " сом, текущий баланс: " + amount + " сом.");
+        } else {
+            System.out.println("Ошибка");
+        }
+    }
+    public void withdraw(double sum) throws LimitException {
+        if (sum > amount) {
+            throw new LimitException("Недостаточно средств!", amount);
+        } else {
+            amount -= sum;
+            System.out.println("Снято: " + sum + ", текущий баланс: " + amount);
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,24 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws InterruptedException {
+        BankAccount account = new BankAccount(15000);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        try {
+            while (true) {
+                account.withdraw(6000);
+            }
+        } catch (LimitException e) {
+            System.out.println("Error: " + e.getMessage());
+            System.out.println("у вас только " + e.getRemainingAmount() + " сом.");
+            System.out.println("выполняется снятие всех денег");
+            Thread.sleep(1000);
+            System.out.println("Loading.");
+            Thread.sleep(1000);
+            System.out.println("Loading..");
+            Thread.sleep(1000);
+            System.out.println("Loading...");
+            Thread.sleep(1000);
+            System.out.println("Успех, ваш баланс снят, текущий баланс: " + (e.getRemainingAmount()  -  e.getRemainingAmount()));
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
         }
     }
 }
